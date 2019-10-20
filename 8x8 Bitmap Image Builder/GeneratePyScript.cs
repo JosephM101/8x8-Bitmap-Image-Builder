c9b6e8994a589419ad57e229991f480fc4e347a0
Joseph .M 101 <jmig3066@gmail.com>
2019-10-20 00:26:48 -0400

    A program that assists in the creation of a bitmap image that's 8x8 in resolution for the Raspberry Pi SenseHAT.

---
+++ 8x8 Bitmap Image Builder/GeneratePyScript.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _8x8_Bitmap_Image_Builder
+{
+    public partial class GeneratePyScript : Form
+    {
+        public GeneratePyScript()
+        {
+            InitializeComponent();
+        }
+
+        private void GeneratePyScript_Load(object sender, EventArgs e)
+        {
+            textBox1.Text=Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text.Replace('\\', '/');
+        }
+    }
+}
